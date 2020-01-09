5f0adb2677ddbb7a2a0c82a1251a4f2a79994b38
Rajasekar <e.rajasekar@gmail.com>
2020-01-09 11:18:03 -0500

    Update deploy with tagging:

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 npm run generate
 aws s3 cp dist s3://astrosoft-ui --recursive
 aws cloudfront create-invalidation  --distribution-id  E1OHEP56ILDFU1  --paths "/*"
+
+git tag $1;
+git push origin --tags;
+
+
