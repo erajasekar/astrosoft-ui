9fc31cadf71e941209a9f94eb334b1c79eee66e2
Rajasekar <e.rajasekar@gmail.com>
2020-01-23 11:49:33 -0500

    Initial template for header

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,82 @@
 <template>
-  <img class="h-24" src="https://static.wixstatic.com/media/fd356f_c3ba9281b9c84ef5b5e9992d74c397c3~mv2.png"> </img>
+  <section class="hero is-warning">
+  <div class="hero-head">
+    <nav class="navbar">
+      <div class="container">
+        <!-- <div class="navbar-brand">
+          <a class="navbar-item">
+            <img class="h-24" src="https://static.wixstatic.com/media/fd356f_c3ba9281b9c84ef5b5e9992d74c397c3~mv2.png" alt="Innovative Astro Solutions"> </img>
+          </a>
+          <span class="navbar-burger burger" data-target="navbarMenuHeroB">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+        -->
+        <div id="navbarMenuHeroB" class="navbar-menu">
+          <div class="navbar-end">
+            <a class="navbar-item is-active">
+              Home
+            </a>
+            <a class="navbar-item">
+              Examples
+            </a>
+            <a class="navbar-item">
+              Documentation
+            </a>
+            <span class="navbar-item">
+              <a class="button is-info is-inverted">
+                <span class="icon">
+                  <i class="fab fa-github"></i>
+                </span>
+                <span>Download</span>
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <img class="h-24" src="https://static.wixstatic.com/media/fd356f_c3ba9281b9c84ef5b5e9992d74c397c3~mv2.png" alt="Innovative Astro Solutions"> </img>
+      <p class="subtitle">
+        Learn Astrology
+      </p>
+    </div>
+  </div>
+
+  <div class="hero-foot">
+    <nav class="tabs is-boxed is-fullwidth">
+      <div class="container">
+        <ul>
+          <li class="is-active">
+            <a>Overview</a>
+          </li>
+          <li>
+            <a>Modifiers</a>
+          </li>
+          <li>
+            <a>Grid</a>
+          </li>
+          <li>
+            <a>Elements</a>
+          </li>
+          <li>
+            <a>Components</a>
+          </li>
+          <li>
+            <a>Layout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</section>
 </template>
+
 <style>
 
 </style>
