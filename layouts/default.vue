90d0fafc10e922cb6d39c975cd5304439940a0bb
Rajasekar <e.rajasekar@gmail.com>
2020-01-23 21:54:39 -0500

    Customized footer and background color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,19 @@
 <template>
-  <section class="center site">
+  <section class="center site bg-orange-100">
     <logo />
     <div class="container main">
       <nuxt />
     </div>
-    <footer class="footer bg-teal-300">
-      <div class="content has-text-centered">
-        <p>
-          &copy;  by <a href="https://www.innovativeastrosolutions.com/">Innovative Astro Solutions</a>.
+    <footer class="p-5 bg-orange-900">
+      <div class="level text-white text-xl font-bold">
+        <p class="level-item">
+          &copy; by &nbsp; <a href="https://www.innovativeastrosolutions.com/"> Innovative Astro Solutions</a>.
+        </p>
+        <p class="level-item">
+          Contact : innovativeastrosolutions@gmail.com
+        </p>
+        <p class="level-item">
+          Phone : +91 9008477228
         </p>
       </div>
     </footer>
