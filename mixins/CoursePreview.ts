609faf8fd80debcd573443a921b3ca6f955bef0e
Rajasekar Elango <e.rajasekar@gmail.com>
2020-04-07 17:48:34 -0400

    Updated course preview

--- mixins/CoursePreview.ts
+++ mixins/CoursePreview.ts
@@ -21,5 +21,45 @@ export const previewVideos : Array<PreviewVideo> = [
   {
     title: 'கர்மவினையும் ஜோதிடமும் ',
     src: 'https://www.youtube.com/embed/u5wBpIbUzkA'
+  },
+  {
+    title: 'Zodiac and its significance ',
+    src: 'https://www.youtube.com/embed/8BJ7B9mAfCw'
+  },
+  {
+    title: 'ராசி மண்டலத்தின் முக்கியத்துவம் ',
+    src: 'https://www.youtube.com/embed/AYBZ9a8Tcys'
+  },
+  {
+    title: 'Rahu, Ketu and Eclipse  ',
+    src: 'https://www.youtube.com/embed/cvDtagXRm-U'
+  },
+  {
+    title: 'கிரகணத்திற்கும், ராகு&கேது விற்கும் உள்ள தொடர்பு ',
+    src: 'https://www.youtube.com/embed/L-a-ljOpfe0'
+  },
+  {
+    title: 'Retrograde Motion (Scientific Explanation) ',
+    src: 'https://www.youtube.com/embed/f6mRhUEm1hU'
+  },
+  {
+    title: 'வக்ரகதி என்றால் என்ன? விஞ்ஞான பூர்வமான விளக்கம் ',
+    src: 'https://www.youtube.com/embed/GWwgfpJACGM'
+  },
+  {
+    title: 'What is Ayanamsa ',
+    src: 'https://www.youtube.com/embed/c28eaEPLcgc'
+  },
+  {
+    title: 'ஜோதிடத்தில் அயனாம்சம் என்றால் என்ன? ',
+    src: 'https://www.youtube.com/embed/9z5hN2Y5WSM'
+  },
+  {
+    title: 'Bhavas in Vedic Astrology ',
+    src: 'https://www.youtube.com/embed/ZxXj5mInoMo'
+  },
+  {
+    title: 'வேத ஜோதிடத்தில் பாவங்கள் என்றால் என்ன? ',
+    src: 'https://www.youtube.com/embed/ofkkvAd1x6k'
   }
 ]
