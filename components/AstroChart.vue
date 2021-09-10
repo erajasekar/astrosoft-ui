0c47177f69d726eecb1856edfaa7305f471fd5e0
hrishijd <hrishi.jadhav24@gmail.com>
2021-09-10 08:38:58 -0700

    added resources via manual template

---
+++ components/AstroChart.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+  export default{
+    data: () => ({
+    }),
+    mounted() {
+      let recaptchaScript = document.createElement('script')
+      recaptchaScript.setAttribute('src', 'https://rawgit.com/erajasekar/astrochartjs/master/dist/astrochart.js')
+      document.head.appendChild(recaptchaScript)
+    }
+  }
+</script>
