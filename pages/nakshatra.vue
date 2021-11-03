e716815b1324c1197568f5ade779eac4a783512e
hrishijd <hrishi.jadhav24@gmail.com>
2021-11-03 14:01:25 +0530

    completed suggested changes in nakshatra

--- pages/nakshatra.vue
+++ pages/nakshatra.vue
@@ -8,7 +8,7 @@
         <label class="label text-gray-600">Rasi</label>
         <select v-model="rasiBoy" @change="calcBoy()" class="rasiBoy card mb-5">
         <option disabled value="0">Please select one</option>
-        <option value="1">Mesh</option>
+        <option value="1">Mesha</option>
         <option value="2">Vrishabha</option>
         <option value="3">Mithuna</option>
         <option value="4">Kataka</option>
@@ -34,7 +34,7 @@
         <label class="label text-gray-600">Rasi</label>
         <select v-model="rasiGirl" @change="calcGirl()" class="card mb-5">
         <option disabled value="0">Please select one</option>
-        <option value="1">Mesh</option>
+        <option value="1">Mesha</option>
         <option value="2">Vrishabha</option>
         <option value="3">Mithuna</option>
         <option value="4">Kataka</option>
@@ -82,13 +82,11 @@
                 <td>{{resp.totalKutasGained.gained}}</td>
                 <td>{{resp.totalKutasGained.max}}</td>
             </tr>
-            <tr>
-                <td colspan="3">
-                Important Note: The star matching is just the first step, don't assume that only star matching is sufficient and fail to check the
-                 overall horoscope for other factors(childbirth, happy family life, divorce possibilities, progeny, financial status, etc).
-                </td>
-            </tr>
         </table>
+        <div class="note card p-4">
+            Important Note: The star matching is just the first step, don't assume that only star matching is sufficient and fail to check the
+            overall horoscope for other factors(childbirth, happy family life, divorce possibilities, progeny, financial status, etc).
+        </div>
     </div>
   </section>
 </template>
@@ -148,7 +146,7 @@ export default class NakshatraVue extends Vue {
 
   head () {
     return {
-      titleTemplate: 'Nakshatra| Innovative Astro Solutions',
+      titleTemplate: 'Nakshatra Compatibility | Innovative Astro Solutions',
       meta: [
         { name: 'og:url', content: getCurrentPageUrl('/nakshatra') },
         { name: 'twitter:url', content: getCurrentPageUrl('/nakshatra') }
@@ -157,56 +155,41 @@ export default class NakshatraVue extends Vue {
   }
 
   calcBoy () {
-    if (this.rasiBoy === '1') {
-      this.nkb1 = 'Ashwini'
-      this.nkb2 = 'Bharani'
-      this.nkb3 = 'Kirthika'
-    } else if (this.rasiBoy === '2') {
-      this.nkb1 = 'Kirthika'
-      this.nkb2 = 'Rohini'
-      this.nkb3 = 'Mrigasira'
-    } else if (this.rasiBoy === '3') {
-      this.nkb1 = 'Mrigsira'
-      this.nkb2 = 'Ardra'
-      this.nkb3 = 'Punarvasu'
-    } else if (this.rasiBoy === '4') {
-      this.nkb1 = 'Punarvasu'
-      this.nkb2 = 'Pushyami'
-      this.nkb3 = 'Ashlesha'
-    } else if (this.rasiBoy === '5') {
-      this.nkb1 = 'Magha'
-      this.nkb2 = 'Purva Phalguni'
-      this.nkb3 = 'Uttra Phalguni'
-    } else if (this.rasiBoy === '6') {
-      this.nkb1 = 'Uttra Phalguni'
-      this.nkb2 = 'Hastra'
-      this.nkb3 = 'Chitra'
-    } else if (this.rasiBoy === '7') {
-      this.nkb1 = 'Chitra'
-      this.nkb2 = 'Swathi'
-      this.nkb3 = 'Vishakha'
-    } else if (this.rasiBoy === '8') {
-      this.nkb1 = 'Vishakha'
-      this.nkb2 = 'Anuradha'
-      this.nkb3 = 'Jyestha'
-    } else if (this.rasiBoy === '9') {
-      this.nkb1 = 'Moola'
-      this.nkb2 = 'Purva Ashadha'
-      this.nkb3 = 'Uttra Ashadha'
-    } else if (this.rasiBoy === '10') {
-      this.nkb1 = 'Uttra Ashadha'
-      this.nkb2 = 'Shravana'
-      this.nkb3 = 'Dhanishta'
-    } else if (this.rasiBoy === '11') {
-      this.nkb1 = 'Dhanishta'
-      this.nkb2 = 'Shatabhisha'
-      this.nkb3 = 'Purva Bhadrapada'
-    } else if (this.rasiBoy === '12') {
-      this.nkb1 = 'Purva Bhadrapada'
-      this.nkb2 = 'Uttra Bhadrapada'
-      this.nkb3 = 'Revathi'
-    }
+    const nakshatraArrray = this.calcNakshatra(this.rasiBoy)
+    this.nkb1 = nakshatraArrray[0]
+    this.nkb2 = nakshatraArrray[1]
+    this.nkb3 = nakshatraArrray[2]
     this.shouldDisable()
+  }
+
+  calcNakshatra (rasi : string) {
+    if (rasi === '1') {
+      return ['Ashwini', 'Bharani', 'Kirthika']
+    } else if (rasi === '2') {
+      return ['Kirthika', 'Rohini', 'Mrigasira']
+    } else if (rasi === '3') {
+      return ['Mrigsira', 'Ardra', 'Punarvasu']
+    } else if (rasi === '4') {
+      return ['Punarvasu', 'Pushyami', 'Ashlesha']
+    } else if (rasi === '5') {
+      return ['Magha', 'Purva Phalguni', 'Uttra Phalguni']
+    } else if (rasi === '6') {
+      return ['Uttra Phalguni', 'Hastra', 'Chitra']
+    } else if (rasi === '7') {
+      return ['Chitra', 'Swathi', 'Vishakha']
+    } else if (rasi === '8') {
+      return ['Vishakha', 'Anuradha', 'Jyestha']
+    } else if (rasi === '9') {
+      return ['Moola', 'Purva Ashadha', 'Uttra Ashadha']
+    } else if (rasi === '10') {
+      return ['Uttra Ashadha', 'Shravana', 'Dhanishta']
+    } else if (rasi === '11') {
+      return ['Dhanishta', 'Shatabhisha', 'Purva Bhadrapada']
+    } else if (rasi === '12') {
+      return ['Purva Bhadrapada', 'Uttra Bhadrapada', 'Revathi']
+    } else {
+      return ['', '', '']
+    }
   }
 
   get nakBoy1 (): string {
@@ -222,64 +205,15 @@ export default class NakshatraVue extends Vue {
   }
 
   calcGirl () {
-    if (this.rasiGirl === '1') {
-      this.nkg1 = 'Ashwini'
-      this.nkg2 = 'Bharani'
-      this.nkg3 = 'Kirthika'
-    } else if (this.rasiGirl === '2') {
-      this.nkg1 = 'Kirthika'
-      this.nkg2 = 'Rohini'
-      this.nkg3 = 'Mrigasira'
-    } else if (this.rasiGirl === '3') {
-      this.nkg1 = 'Mrigsira'
-      this.nkg2 = 'Ardra'
-      this.nkg3 = 'Punarvasu'
-    } else if (this.rasiGirl === '4') {
-      this.nkg1 = 'Punarvasu'
-      this.nkg2 = 'Pushyami'
-      this.nkg3 = 'Ashlesha'
-    } else if (this.rasiGirl === '5') {
-      this.nkg1 = 'Magha'
-      this.nkg2 = 'Purva Phalguni'
-      this.nkg3 = 'Uttra Phalguni'
-    } else if (this.rasiGirl === '6') {
-      this.nkg1 = 'Uttra Phalguni'
-      this.nkg2 = 'Hastra'
-      this.nkg3 = 'Chitra'
-    } else if (this.rasiGirl === '7') {
-      this.nkg1 = 'Chitra'
-      this.nkg2 = 'Swathi'
-      this.nkg3 = 'Vishakha'
-    } else if (this.rasiGirl === '8') {
-      this.nkg1 = 'Vishakha'
-      this.nkg2 = 'Anuradha'
-      this.nkg3 = 'Jyestha'
-    } else if (this.rasiGirl === '9') {
-      this.nkg1 = 'Moola'
-      this.nkg2 = 'Purva Ashadha'
-      this.nkg3 = 'Uttra Ashdha'
-    } else if (this.rasiGirl === '10') {
-      this.nkg1 = 'Uttra Ashadha'
-      this.nkg2 = 'Shravana'
-      this.nkg3 = 'Dhanishta'
-    } else if (this.rasiGirl === '11') {
-      this.nkg1 = 'Dhanishta'
-      this.nkg2 = 'Shatabhisha'
-      this.nkg3 = 'Purva Bhadrapada'
-    } else if (this.rasiGirl === '12') {
-      this.nkg1 = 'Purva Bhadrapada'
-      this.nkg2 = 'Uttra Bhadrapada'
-      this.nkg3 = 'Revathi'
-    }
+    const nakshatraArrray = this.calcNakshatra(this.rasiGirl)
+    this.nkg1 = nakshatraArrray[0]
+    this.nkg2 = nakshatraArrray[1]
+    this.nkg3 = nakshatraArrray[2]
     this.shouldDisable()
   }
 
   shouldDisable () {
-    if (this.rasiBoy !== '0' && this.rasiGirl !== '0') {
-      this.allset = true
-    } else {
-      this.allset = false
-    }
+    this.allset = this.rasiBoy !== '0' && this.rasiGirl !== '0'
   }
 
   get nakGirl1 (): string {
@@ -298,23 +232,23 @@ export default class NakshatraVue extends Vue {
     this.fetchData()
   }
 
-  nakshatraNum (val1: string, val2: string) {
-    /* return ((parseInt(val1) - 1) * 3) + parseInt(val2) */
+  nakshatraNum (rasi: string, nakshatra: string) {
+    const rasiInt = parseInt(rasi)
     let val = 1
-    if (parseInt(val1) === 1) {
+    if (rasiInt === 1) {
       val = 0
-    } else if (parseInt(val1) <= 4) {
-      val = (parseInt(val1) - 1) * 2
-    } else if (parseInt(val1) === 5) {
+    } else if (rasiInt <= 4) {
+      val = (rasiInt - 1) * 2
+    } else if (rasiInt === 5) {
       val = 9
-    } else if (parseInt(val1) <= 8) {
-      val = 9 + (parseInt(val1) - 5) * 2
-    } else if (parseInt(val1) === 9) {
+    } else if (rasiInt <= 8) {
+      val = 9 + (rasiInt - 5) * 2
+    } else if (rasiInt === 9) {
       val = 18
     } else {
-      val = 18 + (parseInt(val1) - 9) * 2
+      val = 18 + (rasiInt - 9) * 2
     }
-    return val + parseInt(val2)
+    return val + parseInt(nakshatra)
   }
 
   async fetchData () {
@@ -330,8 +264,6 @@ export default class NakshatraVue extends Vue {
       }
     }
     const resp = await this.$axios.$post('https://api.innovativeastrosolutions.com/v0/compatibility', body)
-    console.log(body)
-    console.log(resp)
     this.allocate(resp)
     this.valuesVisibility = true
     this.isLoading = false
